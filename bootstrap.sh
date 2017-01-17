238afa643a4f6e0f857fd2cbcee5cd47cce0ca71
Cloud User <ec2-user@ose3master1.services.cs.saas.ca.com>
2017-01-17 21:25:58 +0000

    Updating

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 read -s -p "Enter Github Username: " gitUsername
 read -s -p "\nEnter Github Password: " gitPassword
-oc new-project ci
+#oc new-project ci
+oc adm new-project ci --node-selector=""
+oc project ci
+
 #oc create serviceaccount jenkinsaccount -n ci
 #oadm policy add-scc-to-user privileged system:serviceaccount:ci:jenkinsaccount
 oadm policy add-scc-to-user privileged system:serviceaccount:ci:default
@@ -9,5 +12,5 @@ oc secrets new-basicauth scmsecret --username=$gitUsername --password=$gitPasswo
 oc secrets add serviceaccount/builder secrets/scmsecret
 oc policy add-role-to-user edit system:serviceaccount:ci:default -n ci
 #oc policy add-role-to-user edit system:serviceaccount:ci:jenkinsaccount -n ci
-oc create -n ci -f master/jenkins-master-template.json
+oc create -n ci -f master/jenkins-master-template_persistent.json
 oc create -n ci -f slave/jenkins-slave-template.json
